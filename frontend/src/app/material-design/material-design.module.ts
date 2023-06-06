44d96a749dcbac9acd071561316c5937c775480b
Jakob Fassunge <jakob.fassunge1998@gmail.com>
2023-06-06 16:05:54 +0200

    added comments to code

--- frontend/src/app/material-design/material-design.module.ts
+++ frontend/src/app/material-design/material-design.module.ts
@@ -3,12 +3,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// add import statements here:
 
-
+// needed Material components get added in th exports array
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
+  imports: [CommonModule],
+  exports: [
+
   ]
 })
 export class MaterialDesignModule { }
