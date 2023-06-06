96253c5d499e31c286ad18231dfa1c52645729f3
Jakob Fassunge <jakob.fassunge1998@gmail.com>
2023-06-06 15:59:38 +0200

    added the material design module

---
+++ frontend/src/app/material-design/material-design.module.ts
@@ -0,0 +1,14 @@
+// hier kommen alle benötigten imports von @angular/material hin
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class MaterialDesignModule { }
