85468108de0b7fe80171cababb83acc62334d8f4
Jakob Fassunge <jakob.fassunge1998@gmail.com>
2023-06-09 15:05:52 +0200

    begun work on routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventEditingComponent } from './event-editing/event-editing.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { StoryManagementComponent } from './story-management/story-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'event-editing', component: EventEditingComponent
+  },
+  {
+    path: 'story-management', component: StoryManagementComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
