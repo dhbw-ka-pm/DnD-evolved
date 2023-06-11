a6c12d2704c176e0852f0f4e368d3279a1748946
Jakob Fassunge <jakob.fassunge1998@gmail.com>
2023-06-11 18:01:41 +0200

    added routing to sysstem

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventEditingComponent } from './event-editing/event-editing.component';
-import { NavigationComponent } from './navigation/navigation.component';
 import { StoryManagementComponent } from './story-management/story-management.component';
 
 const routes: Routes = [
