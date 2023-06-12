f03a21da34a8b83659c570789b0a2e31b986c4d7
Jakob Fassunge <jakob.fassunge1998@gmail.com>
2023-06-12 11:30:33 +0200

    added fullscreen toggle

--- frontend/src/app/map-image-view/map-image-view.component.ts
+++ frontend/src/app/map-image-view/map-image-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-map-image-view',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./map-image-view.component.css']
 })
 export class MapImageViewComponent {
+  @ViewChild('imageElement') imageElement!: ElementRef;
+  locations: { x: number, y: number }[] = [];
+
+  toggleFullscreen() {
+    const image = this.imageElement.nativeElement as HTMLImageElement;
+    if (!document.fullscreenElement) {
+      image.requestFullscreen();
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.generateRandomLocations();
+  }
+
+  generateRandomLocations() {
+    const image = this.imageElement.nativeElement as HTMLImageElement;
+    for (let i = 0; i < 5; i++) {
+      const x = Math.random() * image.width;
+      const y = Math.random() * image.height;
+      this.locations.push({ x, y });
+    }
+  }
 
 }
