a90ae5c94dfe9705f1fd0b5b98cd803f471c5f34
Nils Bohland <bohland.nils.a22@student.dhbw-karlsruhe.de>
2023-06-13 13:29:11 +0200

    generated tags removed

--- Backend/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ Backend/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -12,7 +12,6 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@javax.annotation.processing.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2023-06-11T16:16:55.027943953Z[GMT]")
 @Configuration
 public class SwaggerDocumentationConfig {
 
