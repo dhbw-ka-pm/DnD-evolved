5628167d3459bd22cd27a29557a8cd112b5e6666
Jakob Fassunge <jakob.fassunge1998@gmail.com>
2023-07-12 10:18:46 +0200

    started sanitization

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { EventEditingComponent } from './event-editing/event-editing.component';
 import { StoryManagementComponent } from './story-management/story-management.component';
 import { MapViewComponent } from './map-view/map-view.component';
+import { InteractiveMapComponent } from './interactive-map/interactive-map.component';
 
 const routes: Routes = [
   { path: 'event-editing', component: EventEditingComponent },
   { path: 'story-management', component: StoryManagementComponent },
   { path: 'map-view', component: MapViewComponent },
+  { path: 'map', component: InteractiveMapComponent },
 ];
 
 @NgModule({
