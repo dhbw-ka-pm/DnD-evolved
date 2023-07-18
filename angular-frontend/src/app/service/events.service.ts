2e7a1402756a1bd810efaf3a26408d9c296825b9
Nick Möhrmann <moehrmann.n@icloud.com>
2023-07-19 01:41:57 +0200

    get event serials from map api call fix

--- angular-frontend/src/app/service/events.service.ts
+++ angular-frontend/src/app/service/events.service.ts
@@ -13,7 +13,7 @@ export class EventsService {
   }
 
   getEvents(serial: string): any {
-    this.http.get('http://localhost:8080/DnDEvolved/v1/maps/' + serial, { responseType: 'text' }).subscribe((xmlData: string) => {
+    this.http.get('http://localhost:8080/DnDEvolved/v1/maps/events/' + serial, { responseType: 'text' }).subscribe((xmlData: string) => {
         const p: xml2js.Parser = new xml2js.Parser();
         p.parseString(xmlData, (err, result) => {
           if (err) {
@@ -22,8 +22,7 @@ export class EventsService {
           const json =JSON.stringify(result, null, 4);
           console.log("get events service stuff");
           console.log(json)
-
-          return result.Map.Event;
+          return result.events.event;
         });
       });
 
