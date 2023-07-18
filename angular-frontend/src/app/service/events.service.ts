52a278ce982b62088ff3c287f070a6724a962644
Nick Möhrmann <moehrmann.n@icloud.com>
2023-07-19 01:27:11 +0200

    get event serials from map api call fix

--- angular-frontend/src/app/service/events.service.ts
+++ angular-frontend/src/app/service/events.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 import * as xml2js from 'xml2js';
 
 @Injectable({
@@ -9,22 +9,28 @@ export class EventsService {
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor(private http: HttpClient
-             ) { }
+  ) {
+  }
 
   getEvents(): any {
-    this.http.get('http://localhost:8080/DnDEvolved/v1/maps/1078cf56-8ec0-4af7-88c6-0775c9f8307c', { responseType: 'text' }).subscribe((xmlData: string) => {
+    const contentType = new HttpHeaders().set('Content-Type', 'application/xml');
+    this.http.get('http://localhost:8080/DnDEvolved/v1/maps/1078cf56-8ec0-4af7-88c6-0775c9f8307c', { headers: contentType, responseType: "text" }).subscribe(
+      response => {
         const p: xml2js.Parser = new xml2js.Parser();
-        p.parseString(xmlData, (err, result) => {
+        p.parseString(response, (err, result) => {
           if (err) {
             throw err;
           }
-          const json =JSON.stringify(result, null, 4);
+          const json = JSON.stringify(result, null, 4);
           console.log("get events service stuff");
-          console.log(json)
-
+          console.log(json);
+          console.log(response);
           return result.Map.Event;
         });
-      });
+      }
+      , // Handle success here
+      error => console.log(error) // Handle error here
+    );
 
   }
 }
